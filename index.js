d6b8118adf516bace453eb9b3a50e30e30e7f6e3
Tiny Penguin <chase.seeberger@gmail.com>
2024-01-03 14:19:49 -0800

    ammending questions and testing input

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-// TODO: Include packages needed for this application
+// Include packages needed for this application
 const inqurier = require('inquirer');
 
-// TODO: Create an array of questions for user input
+// Create an array of questions for user input
 const questions = [
     // WHEN I enter my project title
     // TODO: THEN this is displayed as the title of the README
@@ -14,42 +14,64 @@ const questions = [
     // TODO: THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
     {
         type: 'input',
-        message: 'Installing Instructions?',
-        name: 'title'
+        message: 'Project Description?',
+        name: 'description'
     },
     {
         type: 'input',
         message: 'Usage Information?',
-        name: 'title'
+        name: 'usage'
     },
     {
         type: 'input',
         message: 'Contribution Guidelines?',
-        name: 'title'
+        name: 'contribution'
     },
     {
         type: 'input',
         message: 'Test Instructions?',
-        name: 'title'
+        name: 'testInstructions'
     },
     {
         type: 'input',
-        message: 'Project Description?',
-        name: 'title'
+        message: 'Installing Instructions?',
+        name: 'install'
     },
+    // WHEN I choose a license for my application from a list of options
+    // TODO: THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+    {
+        type: 'list',
+        message: 'Select a License for your project.',
+        name: 'license',
+        choices: ['MIT', 'Apache', 'GPL', 'BSD', 'None'],
+        default: 'None'
+    },
+    // WHEN I enter my GitHub username
+    // TODO: THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
     {
         type: 'input',
-        message: 'Choose a license?',
-        name: 'title'
+        message: 'GitHub Username?',
+        name: 'github'
     },
-    
+    // WHEN I enter my email address
+    // TODO: THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+    {
+        type: 'input',
+        message: 'Email Address?',
+        name: 'email'
+    }
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+// Create a function to initialize app
+function init() {
+    inqurier.prompt(questions)
+    .then((answers) => {
+        console.log(answers);
+    })
+}
 
 // Function call to initialize app
 init();
@@ -60,11 +82,5 @@ init();
 // WHEN I am prompted for information about my application repository
 // THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
 
-// WHEN I choose a license for my application from a list of options
-// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
-// WHEN I enter my GitHub username
-// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
-// WHEN I enter my email address
-// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
 // WHEN I click on the links in the Table of Contents
 // THEN I am taken to the corresponding section of the README
